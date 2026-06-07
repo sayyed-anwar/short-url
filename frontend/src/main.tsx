37139021e6e110db33f391bcfb205cfd9bc5ff95
Anwar <helloanwar975@gmail.com>
2026-06-07 13:48:48 +0530

    feat: add .gitkeep files to maintain directory structure and import Tailwind CSS in styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./styles/index.css";
 
 import { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
